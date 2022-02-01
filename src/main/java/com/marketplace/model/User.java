5953b786982fba80752edd6bdeed1607cf950b83
Davaious <davidalcalde.hyland@gmail.com>
2022-02-01 15:37:51 +0100

    startback

---
+++ src/main/java/com/marketplace/model/User.java
@@ -0,0 +1,51 @@
+package com.marketplace.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    private string name;
+    private string surname;
+    private string email;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public string getSurname() {
+        return surname;
+    }
+
+    public void setSurname(string surname) {
+        this.surname = surname;
+    }
+
+    public string getEmail() {
+        return email;
+    }
+
+    public void setEmail(string email) {
+        this.email = email;
+    }
+}
