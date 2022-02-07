f9728a40bc631a7a90ce0ec9096c516ad9e65981
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2022-02-07 11:29:55 +0100

    Defaul commit

--- src/main/java/com/marketplace/model/User.java
+++ src/main/java/com/marketplace/model/User.java
@@ -1,9 +1,6 @@
 package com.marketplace.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
@@ -14,6 +11,8 @@ public class User {
     private String surname;
     private String email;
     private String avatarSeller;
+
+
 
     public User() {
     }
