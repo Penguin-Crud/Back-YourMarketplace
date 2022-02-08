82abd45786f80dd18dd0af17cd53a3e72b31d3e4
Facu Ramallo <framallo.ing@gmail.com>
2022-02-08 09:03:51 +0100

    img atribute added for products

--- src/main/java/com/marketplace/model/Product.java
+++ src/main/java/com/marketplace/model/Product.java
@@ -11,6 +11,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+    private String img;
     private float price;
     private String description;
     private String console;
@@ -18,6 +19,22 @@ public class Product {
 
 
     public Product() {
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public int getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(int sellerId) {
+        this.sellerId = sellerId;
     }
 
     public int getId() {
